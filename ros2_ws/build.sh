62cbf79f551713893dfc127d7d258da7d25b98a1
zhaoli <zhaoli.go@gmail.com>
2025-12-11 10:09:10 +0400

    feat: Update build script formatting, add .vscode to gitignore, and update submodule references

--- ros2_ws/build.sh
+++ ros2_ws/build.sh
@@ -1,1 +1,3 @@
-colcon build --symlink-install  --cmake-args -DCMAKE_BUILD_TYPE=Debug   --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --base-paths src/ego-planner-swarm
+colcon build --symlink-install \
+  --cmake-args -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  --base-paths src/ego-planner-swarm
